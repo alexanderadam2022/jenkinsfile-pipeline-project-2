980516257715c93e6ebe1333978cd27c853c6ae8
adam <alexanderadam2022@gmail.com>
2022-09-18 12:59:11 +0300

    updated jenkinsfile and added Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,17 @@
-public class Hello {
-
-    public static void main(String[] args) {
-        System.out.println("Hello from Java");
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            steps {
+                echo 'Compiling the java source code'
+                sh 'javac Hello.java'
+            }
+        }
+        stage('run') {
+            steps {
+                echo 'Running the compiled java code.'
+                sh 'java Hello'
+            }
+        }
     }
 }
